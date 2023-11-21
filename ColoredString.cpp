d1c2cd89d7058882b99e54b8a471f54aa8453b51
Arrash03 <atv1382@gmail.com>
2023-11-21 12:05:23 +0330

    edit include

--- ColoredString.cpp
+++ ColoredString.cpp
@@ -1,13 +1,15 @@
 #include "ColoredString.h"
 
-string ColoredString::red(const string &s) noexcept {return "\033[1;31m" + s + "\033[0m";}
+#include <string>
 
-string ColoredString::green(const string &s) noexcept {return "\033[1;32m" + s + "\033[0m";}
+std::string ColoredString::red(const std::string &s) noexcept {return "\033[1;31m" + s + "\033[0m";}
 
-string ColoredString::yellow(const string &s) noexcept {return "\033[1;33m" + s + "\033[0m";}
+std::string ColoredString::green(const std::string &s) noexcept {return "\033[1;32m" + s + "\033[0m";}
 
-string ColoredString::blue(const string &s) noexcept {return "\033[1;34m" + s + "\033[0m";}
+std::string ColoredString::yellow(const std::string &s) noexcept {return "\033[1;33m" + s + "\033[0m";}
 
-string ColoredString::magenta(const string &s) noexcept {return "\033[1;35m" + s + "\033[0m";}
+std::string ColoredString::blue(const std::string &s) noexcept {return "\033[1;34m" + s + "\033[0m";}
 
-string ColoredString::white(const string &s) noexcept {return "\033[1;37m" + s + "\033[0m";}
+std::string ColoredString::magenta(const std::string &s) noexcept {return "\033[1;35m" + s + "\033[0m";}
+
+std::string ColoredString::white(const std::string &s) noexcept {return "\033[1;37m" + s + "\033[0m";}
